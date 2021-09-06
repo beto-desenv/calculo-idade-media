d985d128cd93d36788a4fb1580e401d73b359268
Roberto Vieira Carlos <beto.vieiracarlos@gmail.com>
2021-09-06 20:08:48 -0300

    Commit inicial

---
+++ calculo-idade-media/calculo-idade-media/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace calculo_idade_media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Double media = 0;
+            Double idade1 = 0;
+            Double idade2 = 0;
+            Double idade3 = 0;
+            Double idade4 = 0;
+            Double idade5 = 0;
+
+            Console.WriteLine("Programa que calcula a idade media de cinco alunos");
+            Console.Write("Infome a idade do 1° aluno: ");
+
+            idade1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe a idade do 2° aluno: ");
+            idade2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe a idade do 3° aluno: ");
+            idade3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe a idade do 4° aluno: ");
+            idade4 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe a idade do 5° aluno: ");
+            idade5 = Convert.ToDouble(Console.ReadLine());
+
+            media = (idade1 + idade2 + idade3 + idade4 + idade5) / 5;
+
+            Console.WriteLine("A idade média dos alunos é " + media);
+            Console.ReadKey();
+        }
+    }
+}
